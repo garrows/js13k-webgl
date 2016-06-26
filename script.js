f6ff1359d697fa25be3f69c18b1cd2a2b1306ba1
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2016-06-27 09:00:03 +1000

    Colored shader with dat.gui

--- script.js
+++ script.js
@@ -18,7 +18,24 @@ function initWebGL( canvas ) {
 }
 
 function start() {
-    var canvas = document.getElementById( "glcanvas" );
+    var data = {
+            x: 200,
+            y: 150,
+            angle: 0,
+            scaleX: 1,
+            scaleY: 1,
+            fps: 0,
+        },
+        gui = new dat.GUI();
+    canvas = document.getElementById( "glcanvas" );
+
+    gui.remember( data );
+    gui.add( data, 'x', 0, 400 ); //.onFinishChange( drawWorld );
+    gui.add( data, 'y', 0, 300 );
+    gui.add( data, 'angle', 0, 2 * Math.PI );
+    gui.add( data, 'scaleX', -5, 5 );
+    gui.add( data, 'scaleY', -5, 5 );
+    gui.add( data, 'fps' ).listen();
 
     // Initialize the GL context
     gl = initWebGL( canvas );
@@ -28,34 +45,137 @@ function start() {
 
     // look up where the vertex data needs to go.
     var positionLocation = gl.getAttribLocation( program, "a_position" );
-    var resolutionLocation = gl.getUniformLocation( program, "u_resolution" );
-    var colorLocation = gl.getUniformLocation( program, "u_color" );
-    // set the resolution
-    gl.uniform2f( resolutionLocation, canvas.width, canvas.height );
 
-    // Create a buffer
+    // lookup uniforms
+    var matrixLocation = gl.getUniformLocation( program, "u_matrix" );
+
+    // Create a buffer.
     var buffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
     gl.enableVertexAttribArray( positionLocation );
     gl.vertexAttribPointer( positionLocation, 2, gl.FLOAT, false, 0, 0 );
 
-    gl.drawArrays( gl.TRIANGLES, 0, 6 );
+    // Set Geometry.
+    setGeometry( gl );
 
-    // draw 50 random rectangles in random colors
-    for ( var ii = 0; ii < 5; ++ii ) {
-        // Setup a random rectangle
-        setRectangle(
-            gl, randomInt( 300 ), randomInt( 300 ), randomInt( 300 ), randomInt( 300 ) );
+    // Draw the scene.
+    var lastTimestamp = 0;
 
-        // Set a random color.
-        gl.uniform4f( colorLocation, Math.random(), Math.random(), Math.random(), 1 );
+    function drawScene( timestamp ) {
+        var dt = timestamp - lastTimestamp;
+        lastTimestamp = timestamp;
+        data.fps = 1000 / dt;
 
-        // Draw the rectangle.
-        gl.drawArrays( gl.TRIANGLES, 0, 6 );
+        // Clear the canvas.
+        gl.clear( gl.COLOR_BUFFER_BIT );
+
+        // Compute the matrices
+        var projectionMatrix = make2DProjection( canvas.clientWidth, canvas.clientHeight );
+        var translationMatrix = makeTranslation( data.x, data.y );
+        var rotationMatrix = makeRotation( data.angle );
+        var scaleMatrix = makeScale( data.scaleX, data.scaleY );
+
+        // Multiply the matrices.
+        var matrix = matrixMultiply( scaleMatrix, rotationMatrix );
+        matrix = matrixMultiply( matrix, translationMatrix );
+        matrix = matrixMultiply( matrix, projectionMatrix );
+
+        // Set the matrix.
+        gl.uniformMatrix3fv( matrixLocation, false, matrix );
+
+        // Draw the geometry.
+        gl.drawArrays( gl.TRIANGLES, 0, 3 );
+
+        requestAnimationFrame( drawScene );
     }
 
+    requestAnimationFrame( drawScene );
 }
 
+function make2DProjection( width, height ) {
+    // Note: This matrix flips the Y axis so that 0 is at the top.
+    return [
+        2 / width, 0, 0,
+        0, -2 / height, 0, -1, 1, 1
+    ];
+}
+
+function makeTranslation( tx, ty ) {
+    return [
+        1, 0, 0,
+        0, 1, 0,
+        tx, ty, 1
+    ];
+}
+
+function makeRotation( angleInRadians ) {
+    var c = Math.cos( angleInRadians );
+    var s = Math.sin( angleInRadians );
+    return [
+        c, -s, 0,
+        s, c, 0,
+        0, 0, 1
+    ];
+}
+
+/**
+ * Takes twoMatrix3s, a and b, and computes the product in the order
+ * that pre-composes b with a.  In other words, the matrix returned will
+ * @param {module:webgl-2d-math.Matrix3} a A matrix.
+ * @param {module:webgl-2d-math.Matrix3} b A matrix.
+ * @return {module:webgl-2d-math.Matrix3} the result.
+ * @memberOf module:webgl-2d-math
+ */
+function matrixMultiply( a, b ) {
+    var a00 = a[ 0 * 3 + 0 ];
+    var a01 = a[ 0 * 3 + 1 ];
+    var a02 = a[ 0 * 3 + 2 ];
+    var a10 = a[ 1 * 3 + 0 ];
+    var a11 = a[ 1 * 3 + 1 ];
+    var a12 = a[ 1 * 3 + 2 ];
+    var a20 = a[ 2 * 3 + 0 ];
+    var a21 = a[ 2 * 3 + 1 ];
+    var a22 = a[ 2 * 3 + 2 ];
+    var b00 = b[ 0 * 3 + 0 ];
+    var b01 = b[ 0 * 3 + 1 ];
+    var b02 = b[ 0 * 3 + 2 ];
+    var b10 = b[ 1 * 3 + 0 ];
+    var b11 = b[ 1 * 3 + 1 ];
+    var b12 = b[ 1 * 3 + 2 ];
+    var b20 = b[ 2 * 3 + 0 ];
+    var b21 = b[ 2 * 3 + 1 ];
+    var b22 = b[ 2 * 3 + 2 ];
+    return [
+        a00 * b00 + a01 * b10 + a02 * b20,
+        a00 * b01 + a01 * b11 + a02 * b21,
+        a00 * b02 + a01 * b12 + a02 * b22,
+        a10 * b00 + a11 * b10 + a12 * b20,
+        a10 * b01 + a11 * b11 + a12 * b21,
+        a10 * b02 + a11 * b12 + a12 * b22,
+        a20 * b00 + a21 * b10 + a22 * b20,
+        a20 * b01 + a21 * b11 + a22 * b21,
+        a20 * b02 + a21 * b12 + a22 * b22,
+    ];
+}
+
+function makeScale( sx, sy ) {
+    return [
+        sx, 0, 0,
+        0, sy, 0,
+        0, 0, 1
+    ];
+}
+
+// Fill the buffer with the values that define a triangle.
+function setGeometry( gl ) {
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array( [
+            0, -100,
+            150, 125, -175, 100
+        ] ),
+        gl.STATIC_DRAW );
+}
 // Returns a random integer from 0 to range - 1.
 function randomInt( range ) {
     return Math.floor( Math.random() * range );
--- shader.vert
+++ shader.vert
@@ -1,14 +1,15 @@
 attribute vec2 a_position;
-uniform vec2 u_resolution;
+
+uniform mat3 u_matrix;
+
+varying vec4 v_color;
 
 void main() {
-  vec2 zeroToOne = a_position / u_resolution;
+  // Multiply the position by the matrix.
+  gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
 
-  // convert from 0->1 to 0->2
-  vec2 zeroToTwo = zeroToOne * 2.0;
-
-  // convert from 0->2 to -1->+1 (clipspace)
-  vec2 clipSpace = zeroToTwo - 1.0;
-
-  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+  // Convert from clipspace to colorspace.
+  // Clipspace goes -1.0 to +1.0
+  // Colorspace goes from 0.0 to 1.0
+  v_color = gl_Position * 0.5 + 0.5;
 }
