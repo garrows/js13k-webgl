a936714af54924ea7e3632a68ef7c06e1db68ea5
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2016-06-29 09:07:03 +1000

    Removed utils and size

--- script.js
+++ script.js
@@ -1,132 +1,134 @@
-var gl; // A global variable for the WebGL context
+/* global dat, utils, requestAnimationFrame, alert */
 
-function initWebGL( canvas ) {
-    gl = null;
+var data = { // eslint-disable-line
+    x: 200,
+    y: 150,
+    angle: 0,
+    scaleX: 1,
+    scaleY: 1,
+    fps: 0
+  },
+  gui = new dat.GUI(),
+  canvas = document.getElementById('glcanvas'),
+  gl = canvas.getContext('webgl')
 
-    try {
-        // Try to grab the standard context. If it fails, fallback to experimental.
-        gl = canvas.getContext( "webgl" ) || canvas.getContext( "experimental-webgl" );
-    } catch ( e ) {}
+gui.remember(data)
+gui.add(data, 'x', 0, 400)
+gui.add(data, 'y', 0, 300)
+gui.add(data, 'angle', 0, 2 * Math.PI)
+gui.add(data, 'scaleX', -5, 5)
+gui.add(data, 'scaleY', -5, 5)
+gui.add(data, 'fps').listen()
 
-    // If we don't have a GL context, give up now
-    if ( !gl ) {
-        alert( "Unable to initialize WebGL. Your browser may not support it." );
-        gl = null;
-    }
+var program = gl.createProgram()
+var newShader = function (id, type) {
+  var shader = gl.createShader(type)
 
-    return gl;
+  // wire up the shader and compile
+  gl.shaderSource(shader, document.getElementById(id).import.body.innerText)
+  gl.compileShader(shader)
+
+  // if things didn't go so well alert
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(shader))
+    return null
+  }
+  gl.attachShader(program, shader)
+}
+newShader('vert', gl.VERTEX_SHADER)
+newShader('frag', gl.FRAGMENT_SHADER)
+
+gl.linkProgram(program)
+
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+  alert('Could not initialise main shaders')
 }
 
-function start() {
-    var data = {
-            x: 200,
-            y: 150,
-            angle: 0,
-            scaleX: 1,
-            scaleY: 1,
-            fps: 0,
-        },
-        gui = new dat.GUI();
-    canvas = document.getElementById( "glcanvas" );
+gl.useProgram(program)
 
-    gui.remember( data );
-    gui.add( data, 'x', 0, 400 ); //.onFinishChange( drawWorld );
-    gui.add( data, 'y', 0, 300 );
-    gui.add( data, 'angle', 0, 2 * Math.PI );
-    gui.add( data, 'scaleX', -5, 5 );
-    gui.add( data, 'scaleY', -5, 5 );
-    gui.add( data, 'fps' ).listen();
+// look up where the vertex data needs to go.
+var positionLocation = gl.getAttribLocation(program, 'a_position')
+var colorLocation = gl.getAttribLocation(program, 'a_color')
 
-    // Initialize the GL context
-    gl = initWebGL( canvas );
+// lookup uniforms
+var matrixLocation = gl.getUniformLocation(program, 'u_matrix')
 
-    var program = utils.addShaderProg( gl, 'shader.vert.glsl', 'shader.frag.glsl' );
-    gl.useProgram( program );
+// Create a buffer.
+var buffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+gl.enableVertexAttribArray(positionLocation)
+gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0)
 
-    // look up where the vertex data needs to go.
-    var positionLocation = gl.getAttribLocation( program, "a_position" );
-    var colorLocation = gl.getAttribLocation( program, "a_color" );
+// Set Geometry.
+setGeometry(gl)
 
-    // lookup uniforms
-    var matrixLocation = gl.getUniformLocation( program, "u_matrix" );
+// Create a buffer for the colors.
+buffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+gl.enableVertexAttribArray(colorLocation)
+gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0)
 
-    // Create a buffer.
-    var buffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
-    gl.enableVertexAttribArray( positionLocation );
-    gl.vertexAttribPointer( positionLocation, 2, gl.FLOAT, false, 0, 0 );
+// Set the colors
+setColors(gl)
 
-    // Set Geometry.
-    setGeometry( gl );
+// Draw the scene.
+var lastTimestamp = 0
 
-    // Create a buffer for the colors.
-    var buffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
-    gl.enableVertexAttribArray( colorLocation );
-    gl.vertexAttribPointer( colorLocation, 4, gl.FLOAT, false, 0, 0 );
-    //Set the colors
-    setColors( gl );
+function drawScene (timestamp) {
+  var dt = timestamp - lastTimestamp
+  lastTimestamp = timestamp
+  data.fps = 1000 / dt
 
-    // Draw the scene.
-    var lastTimestamp = 0;
+  // Clear the canvas.
+  gl.clear(gl.COLOR_BUFFER_BIT)
 
-    function drawScene( timestamp ) {
-        var dt = timestamp - lastTimestamp;
-        lastTimestamp = timestamp;
-        data.fps = 1000 / dt;
+  // Compute the matrices
+  var projectionMatrix = make2DProjection(canvas.clientWidth, canvas.clientHeight)
+  var translationMatrix = makeTranslation(data.x, data.y)
+  var rotationMatrix = makeRotation(data.angle)
+  var scaleMatrix = makeScale(data.scaleX, data.scaleY)
 
-        // Clear the canvas.
-        gl.clear( gl.COLOR_BUFFER_BIT );
+  // Multiply the matrices.
+  var matrix = matrixMultiply(scaleMatrix, rotationMatrix)
+  matrix = matrixMultiply(matrix, translationMatrix)
+  matrix = matrixMultiply(matrix, projectionMatrix)
 
-        // Compute the matrices
-        var projectionMatrix = make2DProjection( canvas.clientWidth, canvas.clientHeight );
-        var translationMatrix = makeTranslation( data.x, data.y );
-        var rotationMatrix = makeRotation( data.angle );
-        var scaleMatrix = makeScale( data.scaleX, data.scaleY );
+  // Set the matrix.
+  gl.uniformMatrix3fv(matrixLocation, false, matrix)
 
-        // Multiply the matrices.
-        var matrix = matrixMultiply( scaleMatrix, rotationMatrix );
-        matrix = matrixMultiply( matrix, translationMatrix );
-        matrix = matrixMultiply( matrix, projectionMatrix );
+  // Draw the geometry.
+  // gl.drawArrays( gl.TRIANGLES, 0, 3 )
+  gl.drawArrays(gl.TRIANGLES, 0, 6)
 
-        // Set the matrix.
-        gl.uniformMatrix3fv( matrixLocation, false, matrix );
-
-        // Draw the geometry.
-        // gl.drawArrays( gl.TRIANGLES, 0, 3 );
-        gl.drawArrays( gl.TRIANGLES, 0, 6 );
-
-
-        requestAnimationFrame( drawScene );
-    }
-
-    requestAnimationFrame( drawScene );
+  requestAnimationFrame(drawScene)
 }
 
-function make2DProjection( width, height ) {
-    // Note: This matrix flips the Y axis so that 0 is at the top.
-    return [
-        2 / width, 0, 0,
-        0, -2 / height, 0, -1, 1, 1
-    ];
+requestAnimationFrame(drawScene)
+
+function make2DProjection (width, height) {
+  // Note: This matrix flips the Y axis so that 0 is at the top.
+  return [
+    2 / width, 0, 0,
+    0, -2 / height, 0, -1, 1, 1
+  ]
 }
 
-function makeTranslation( tx, ty ) {
-    return [
-        1, 0, 0,
-        0, 1, 0,
-        tx, ty, 1
-    ];
+function makeTranslation (tx, ty) {
+  return [
+    1, 0, 0,
+    0, 1, 0,
+    tx, ty, 1
+  ]
 }
 
-function makeRotation( angleInRadians ) {
-    var c = Math.cos( angleInRadians );
-    var s = Math.sin( angleInRadians );
-    return [
-        c, -s, 0,
-        s, c, 0,
-        0, 0, 1
-    ];
+function makeRotation (angleInRadians) {
+  var c = Math.cos(angleInRadians)
+  var s = Math.sin(angleInRadians)
+  return [
+    c, -s, 0,
+    s, c, 0,
+    0, 0, 1
+  ]
 }
 
 /**
@@ -137,109 +139,78 @@ function makeRotation( angleInRadians ) {
  * @return {module:webgl-2d-math.Matrix3} the result.
  * @memberOf module:webgl-2d-math
  */
-function matrixMultiply( a, b ) {
-    var a00 = a[ 0 * 3 + 0 ];
-    var a01 = a[ 0 * 3 + 1 ];
-    var a02 = a[ 0 * 3 + 2 ];
-    var a10 = a[ 1 * 3 + 0 ];
-    var a11 = a[ 1 * 3 + 1 ];
-    var a12 = a[ 1 * 3 + 2 ];
-    var a20 = a[ 2 * 3 + 0 ];
-    var a21 = a[ 2 * 3 + 1 ];
-    var a22 = a[ 2 * 3 + 2 ];
-    var b00 = b[ 0 * 3 + 0 ];
-    var b01 = b[ 0 * 3 + 1 ];
-    var b02 = b[ 0 * 3 + 2 ];
-    var b10 = b[ 1 * 3 + 0 ];
-    var b11 = b[ 1 * 3 + 1 ];
-    var b12 = b[ 1 * 3 + 2 ];
-    var b20 = b[ 2 * 3 + 0 ];
-    var b21 = b[ 2 * 3 + 1 ];
-    var b22 = b[ 2 * 3 + 2 ];
-    return [
-        a00 * b00 + a01 * b10 + a02 * b20,
-        a00 * b01 + a01 * b11 + a02 * b21,
-        a00 * b02 + a01 * b12 + a02 * b22,
-        a10 * b00 + a11 * b10 + a12 * b20,
-        a10 * b01 + a11 * b11 + a12 * b21,
-        a10 * b02 + a11 * b12 + a12 * b22,
-        a20 * b00 + a21 * b10 + a22 * b20,
-        a20 * b01 + a21 * b11 + a22 * b21,
-        a20 * b02 + a21 * b12 + a22 * b22,
-    ];
+function matrixMultiply (a, b) {
+  var a00 = a[ 0 * 3 + 0 ]
+  var a01 = a[ 0 * 3 + 1 ]
+  var a02 = a[ 0 * 3 + 2 ]
+  var a10 = a[ 1 * 3 + 0 ]
+  var a11 = a[ 1 * 3 + 1 ]
+  var a12 = a[ 1 * 3 + 2 ]
+  var a20 = a[ 2 * 3 + 0 ]
+  var a21 = a[ 2 * 3 + 1 ]
+  var a22 = a[ 2 * 3 + 2 ]
+  var b00 = b[ 0 * 3 + 0 ]
+  var b01 = b[ 0 * 3 + 1 ]
+  var b02 = b[ 0 * 3 + 2 ]
+  var b10 = b[ 1 * 3 + 0 ]
+  var b11 = b[ 1 * 3 + 1 ]
+  var b12 = b[ 1 * 3 + 2 ]
+  var b20 = b[ 2 * 3 + 0 ]
+  var b21 = b[ 2 * 3 + 1 ]
+  var b22 = b[ 2 * 3 + 2 ]
+  return [
+    a00 * b00 + a01 * b10 + a02 * b20,
+    a00 * b01 + a01 * b11 + a02 * b21,
+    a00 * b02 + a01 * b12 + a02 * b22,
+    a10 * b00 + a11 * b10 + a12 * b20,
+    a10 * b01 + a11 * b11 + a12 * b21,
+    a10 * b02 + a11 * b12 + a12 * b22,
+    a20 * b00 + a21 * b10 + a22 * b20,
+    a20 * b01 + a21 * b11 + a22 * b21,
+    a20 * b02 + a21 * b12 + a22 * b22
+  ]
 }
 
-function makeScale( sx, sy ) {
-    return [
-        sx, 0, 0,
-        0, sy, 0,
-        0, 0, 1
-    ];
+function makeScale (sx, sy) {
+  return [
+    sx, 0, 0,
+    0, sy, 0,
+    0, 0, 1
+  ]
 }
 
-// Fill the buffer with the values that define a triangle.
-// function setGeometry( gl ) {
-//     gl.bufferData(
-//         gl.ARRAY_BUFFER,
-//         new Float32Array( [
-//             0, -100,
-//             150, 125, -175, 100
-//         ] ),
-//         gl.STATIC_DRAW );
-// }
 // Fill the buffer with the values that define a rectangle.
-function setGeometry( gl ) {
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array( [ -150, -100,
-            150, -100, -150, 100,
-            150, -100, -150, 100,
-            150, 100
-        ] ),
-        gl.STATIC_DRAW );
-}
-
-// Returns a random integer from 0 to range - 1.
-function randomInt( range ) {
-    return Math.floor( Math.random() * range );
-}
-
-// Fills the buffer with the values that define a rectangle.
-function setRectangle( gl, x, y, width, height ) {
-    var x1 = x;
-    var x2 = x + width;
-    var y1 = y;
-    var y2 = y + height;
-    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( [
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2
-    ] ), gl.STATIC_DRAW );
+function setGeometry (gl) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([ -150, -100,
+      150, -100, -150, 100,
+      150, -100, -150, 100,
+      150, 100
+    ]),
+    gl.STATIC_DRAW)
 }
 
 // Fill the buffer with colors for the 2 triangles
 // that make the rectangle.
-function setColors( gl ) {
-    // Pick 2 random colors.
-    var r1 = Math.random();
-    var b1 = Math.random();
-    var g1 = Math.random();
-    var r2 = Math.random();
-    var b2 = Math.random();
-    var g2 = Math.random();
+function setColors (gl) {
+  // Pick 2 random colors.
+  var r1 = Math.random()
+  var b1 = Math.random()
+  var g1 = Math.random()
+  var r2 = Math.random()
+  var b2 = Math.random()
+  var g2 = Math.random()
 
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(
-            [ 0, 0, 0, 1,
-                r1, b1, g1, 1,
-                r1, b1, g1, 1,
-                r2, b2, g2, 1,
-                r2, b2, g2, 1,
-                0, 0, 0, 1,
-            ] ),
-        gl.STATIC_DRAW );
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(
+      [ 0, 0, 0, 1,
+        r1, b1, g1, 1,
+        r1, b1, g1, 1,
+        r2, b2, g2, 1,
+        r2, b2, g2, 1,
+        0, 0, 0, 1
+      ]),
+    gl.STATIC_DRAW)
 }
